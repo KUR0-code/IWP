815490ddbbc4dc952d4f234b6b53b82fe42ad83e
暗がり <88997840+KUR0-code@users.noreply.github.com>
2023-05-28 12:43:02 +0800

    Door fixed

--- Assets/Scripts/Interactables/Keypad.cs
+++ Assets/Scripts/Interactables/Keypad.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Keypad : Interactable
 {
     public GameObject door;
-
+    private bool doorOpen;
     protected override void Interact()
     {
-      
+        doorOpen = !doorOpen;
+        door.GetComponent<Animator>().SetBool("isOpen", doorOpen);
     }
 }
